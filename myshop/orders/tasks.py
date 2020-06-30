bb7e0b603b15de5f8fd5d1bae6f3ae6019304519
Darkhan Akmyrza <darkhanakmyrza1@gmail.com>
2020-06-30 13:40:13 +0500

    orders

--- myshop/orders/tasks.py
+++ myshop/orders/tasks.py
@@ -1,20 +1,20 @@
 from celery import task
-from django.core.mail import send_mail
+
 from .models import Order
+from django.core.mail import send_mail
 from myshop.settings import EMAIL_HOST_USER
 
 
 @task
 def order_created(order_id):
-    """ Oтправки email-уведомлений при успешном оформлении заказа. """
+    """Задача отправки email-уведомлений при успешном оформлении заказа."""
     order = Order.objects.get(id=order_id)
     subject = 'Order nr. {}'.format(order.id)
-    recepient = str(sub['email'].value())
-    message = 'Dear {},\n\n You have succesfully placed an order.\
-               You order id is {}.'.format(order.first_name, order.id)
-    
-    mail_sent = send_mail(subject, 
-                          message,
-                          EMAIL_HOST_USER, 
-                          [order.email])
+    message = 'Dear {},\n\nYou have successfully placed an order.\
+               Your order id is {}.'.format(order.first_name,
+               order.id)
+    mail_sent = send_mail(subject,
+    message,
+    EMAIL_HOST_USER,
+    [str(order.email)],fail_silently = False)
     return mail_sent
