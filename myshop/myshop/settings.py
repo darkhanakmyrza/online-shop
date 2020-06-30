db987eb1397db4f93cdb215e1c781d18c7930736
Darkhan Akmyrza <darkhanakmyrza1@gmail.com>
2020-06-30 13:50:11 +0500

    changes in settings.py

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -132,8 +132,8 @@ CART_SESSION_ID = 'cart'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'blogbacksdu@gmail.com'
-EMAIL_HOST_PASSWORD = 'qmpzAaa01'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
